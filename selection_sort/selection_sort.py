36e5cc5d411bda302c0d3d5a5786b9c7d8abb4d6
Drashtee Parmar <drashteeparmar@gmail.com>
2025-02-18 19:02:59 -0500

    Selection sort code

---
+++ selection_sort/selection_sort.py
@@ -0,0 +1,17 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i  # Assume current element is the smallest
+        for j in range(i + 1, n):  # Find the actual smallest element
+            if arr[j] < arr[min_index]:
+                min_index = j  # Update index of the minimum element
+        #         Swap elements using temp variables
+        temp = arr[i]
+        arr[i] = arr[min_index]
+        arr[min_index] = temp
+
+
+arr = [7, 3, 9, 2, 5]
+# arr = [5, 8, 3, 9, 4, 1, 7]
+selection_sort(arr)
+print("Sorted array:", arr)
